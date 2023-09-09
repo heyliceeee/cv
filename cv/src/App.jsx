356d2ab527817649c637ba8a918a4826a2f78043
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-09 11:39:30 +0100

    💄 feat: Estilização SASS iniciais

--- cv/src/App.jsx
+++ cv/src/App.jsx
@@ -1,8 +1,14 @@
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
+import './styles/components/app.sass';
+
 function App() {
 
   return (
-    <div className="App">
-      <h1>React + SASS</h1>
+    <div id="portfolio">
+      <h1>Alice Dias</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   )
 }
