3474bbd6be63ac8a1b360e2b70d4f2b2006c0098
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-10 19:55:28 +0100

    💄 feat: Estilização SASS da side bar

--- cv/src/components/Sidebar.jsx
+++ cv/src/components/Sidebar.jsx
@@ -1,7 +1,18 @@
+import '../styles/components/sidebar.sass'
+
+import Avatar from '../img/profile.jpg'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside id="sidebar">
+      <img src={Avatar} alt="Alice Dias"/>
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>Informações de contacto</p>
+      <a href="" className="btn">
+        Download currículo
+      </a>
+    </aside>
   )
 }
 
