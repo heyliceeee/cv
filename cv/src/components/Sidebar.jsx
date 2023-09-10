38b35d3889bd4bf2b3f192f17e00307347012803
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-10 23:44:33 +0100

    💄 feat: Estilização SASS da sidebar - redes sociais

--- cv/src/components/Sidebar.jsx
+++ cv/src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
+import Avatar from '../img/profile.jpg'
+
 import '../styles/components/sidebar.sass'
 
-import Avatar from '../img/profile.jpg'
+import SocialNetworks from './SocialNetworks'
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Alice Dias"/>
       <p className="title">Desenvolvedor</p>
-      <p>redes sociais</p>
+      <SocialNetworks />
       <p>Informações de contacto</p>
-      <a href="" className="btn">
+      <a href="#" className="btn">
         Download currículo
       </a>
     </aside>
