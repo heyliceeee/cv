7920cfdb0143322e3d25f4a0088e12dd201d8a61
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-11 01:53:26 +0100

    💄 feat: Estilização SASS do conteúdo principal - projetos

--- cv/src/components/ProjectsContainer.jsx
+++ cv/src/components/ProjectsContainer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import '../styles/components/projectscontainer.sass'
 
 const ProjectsContainer = () => {
   return (
-    <div>ProjectsContainer</div>
+    <section className='projects-container'>
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac conubia at eros neque tincidunt ultrices quis nisi vivamus. Arcu venenatis ipsum odio morbi posuere tortor iaculis laoreet ut.
+      </p>
+      <a href='#' className='btn'>Ver Projetos</a>
+    </section>
   )
 }
 
