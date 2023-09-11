44cca7497abda7d6cb9b55ee57009c420b98e918
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-11 01:38:39 +0100

    💄 feat: Estilização SASS do conteúdo principal

--- cv/src/components/MainContent.jsx
+++ cv/src/components/MainContent.jsx
@@ -1,9 +1,17 @@
 import '../styles/components/maincontent.sass'
 
+import AboutContainer from './AboutContainer'
+import TecnologiesContainer from './TecnologiesContainer'
+import ProjectsContainer from './ProjectsContainer'
+
 
 const MainContent = () => {
   return (
-    <main id="main-content">MainContent</main>
+    <main id="main-content">
+      <AboutContainer />
+      <TecnologiesContainer />
+      <ProjectsContainer />
+    </main>
   )
 }
 
