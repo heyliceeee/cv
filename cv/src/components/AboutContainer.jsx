f84936b094197a115b3143335675faa4846130c5
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-11 01:49:57 +0100

    💄 feat: Estilização SASS do conteúdo principal - sobre

--- cv/src/components/AboutContainer.jsx
+++ cv/src/components/AboutContainer.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import '../styles/components/aboutcontainer.sass'
 
 const AboutContainer = () => {
   return (
-    <div>AboutContainer</div>
+    <section className='about-container'>
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac conubia at eros neque tincidunt ultrices quis nisi vivamus. Arcu venenatis ipsum odio morbi posuere tortor iaculis laoreet ut.
+      </p>
+      <p>
+        Tempor aenean purus morbi suspendisse mattis enim. Praesent placerat sit turpis id fringilla. Consectetur ipsum vel scelerisque vel est quis faucibus nisl turpis praesent non vivamus interdum. Lorem commodo felis placerat dictum proin aliquet tempor varius lacus tortor. Aliquam ullamcorper risus sollicitudin interdum platea.
+      </p>
+    </section>
   )
 }
 
